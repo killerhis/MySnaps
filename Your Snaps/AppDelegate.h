e717cacd92ae566a25f04b345b621e70966a0d31
Hicham Chourak <hichamchourak@Hichams-Mac.local>
2014-07-20 21:36:30 +0200

    Initial Commit

---
+++ Your Snaps/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Your Snaps
+//
+//  Created by Hicham Chourak on 20/07/14.
+//  Copyright (c) 2014 Hicham Chourak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Your Snaps/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Your Snaps
+//
+//  Created by Hicham Chourak on 20/07/14.
+//  Copyright (c) 2014 Hicham Chourak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
